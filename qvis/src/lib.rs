be3091c38e3b222fcddbc92d5a2afc7cb2203676
Henry Rovnyak <h.rovnyak@proton.me>
2026-01-13 21:51:08 -0500

    refactor(qvis): Remove the sticker matching code and just require the matching to be provided

--- qvis/src/lib.rs
+++ qvis/src/lib.rs
@@ -1,68 +1,59 @@
-// `CVState` should be sealed
-#![allow(private_bounds)]
-
 use std::sync::Arc;
 
 use internment::ArcIntern;
 use puzzle_theory::{permutations::Permutation, puzzle_geometry::PuzzleGeometry};
 
-use crate::puzzle_matching::Matcher;
+use crate::{inference::Inference, puzzle_matching::Matcher};
 
-mod cv;
-pub use cv::{assignment::AssigningPixels, inference::Inference};
+mod inference;
 pub mod puzzle_matching;
 
-trait CVState {
-    fn calibrate(&mut self, image: &[(f64, f64, f64)], state: Permutation);
-}
-
 /// Processes images for computer vision
-pub struct CVProcessor<S: CVState> {
-    puzzle: Arc<PuzzleGeometry>,
+pub struct CVProcessor {
     image_size: usize,
     matcher: Matcher,
-    state: S,
+    inference: Inference,
 }
 
-impl CVProcessor<AssigningPixels> {
+pub enum Pixel {
+    Unassigned,
+    WhiteBalance(ArcIntern<str>),
+    Sticker(usize),
+}
+
+impl CVProcessor {
     /// Create a new `CVProcessor` that recognizes the given puzzle in images. `image_size` specifies the number of pixels in the image. The CV algorithm does not care about rows and columns.
     ///
-    /// # Mask
+    /// # Assignment
     ///
-    /// The mask is the same size as the image.
+    /// The assignment is the same size as the image.
     ///
-    /// Each pixel is configured with a color determining which face of the puzzle it belongs to, where the colors are colors of the faces. This method panics if any colors are not faces of the puzzle. The boolean parameter determines whether the pixel should be treated as white balance for the given face: `false` means that it is not white balance and `true` means that it is white balance.
+    /// Each pixel is configured with a number determining which index sticker of the puzzle it belongs to. This method panics if any indices are out of range. The boolean parameter determines whether the pixel should be treated as white balance for the given face: `false` means that it is not white balance and `true` means that it is white balance.
     ///
     /// White balance points should be selected such that the face is parallel with the face that it is acting as white balance for.
     ///
     /// Pixels marked `None` will not be considered in the CV algorithm.
-    pub fn new(puzzle: Arc<PuzzleGeometry>, image_size: usize, mask: Box<[Option<(ArcIntern<str>, bool)>]>) -> CVProcessor<AssigningPixels> {
+    pub fn new(
+        puzzle: Arc<PuzzleGeometry>,
+        image_size: usize,
+        assignment: Box<[Pixel]>,
+    ) -> CVProcessor {
         CVProcessor {
-            puzzle: Arc::clone(&puzzle),
             image_size,
-            matcher: Matcher::new(Arc::clone(&puzzle)),
-            state: AssigningPixels::new(mask, &puzzle),
+            inference: Inference::new(assignment, &puzzle),
+            matcher: Matcher::new(puzzle),
         }
     }
 
-    /// Finish assignment of pixels to stickers
-    pub fn finish_selection(self) -> CVProcessor<Inference> {
-        todo!()
-    }
-}
-
-impl<S: CVState> CVProcessor<S> {
     /// Calibrate the CV processor with an image of the puzzle in the given state.
     pub fn calibrate(&mut self, image: &[(f64, f64, f64)], state: Permutation) {
         assert_eq!(self.image_size, image.len());
 
-        self.state.calibrate(image, state);
+        self.inference.calibrate(image, state);
     }
-}
 
-impl CVProcessor<Inference> {
     /// Process an image and return the most likely state that the puzzle appears to be in, along with the confidence in the prediction. This is guaranteed to be a valid member of the group.
     pub fn process_image(&self, image: Box<[(f64, f64, f64)]>) -> (Permutation, f64) {
-        self.matcher.most_likely(&self.state.infer(&image))
+        self.matcher.most_likely(&self.inference.infer(&image))
     }
 }
